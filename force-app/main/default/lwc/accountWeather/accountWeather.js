c066d702f96a241f21406946d6cf54381bc98062
apostolstefan98 <apostol123s@gmail.com>
2022-01-29 14:33:36 +0200

    reactiveLWC

--- force-app/main/default/lwc/accountWeather/accountWeather.js
+++ force-app/main/default/lwc/accountWeather/accountWeather.js
@@ -1,13 +1,49 @@
 import { LightningElement, track , api, wire } from 'lwc';
 import retrieveWeather from "@salesforce/apex/WeatherController.retrieveWeather";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getRecord } from 'lightning/uiRecordApi';
+const FIELDS  = ['Account.Id','Account.BillingCity'];
 
 export default class AccountWeather extends LightningElement {
     @api recordId;
     @track result = [];
+    account;
+    lastAddress;
 
     async connectedCallback(){
         this.retrieveWeather();
     }
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    wiredRecord({ error, data }) {
+        if (error) {
+           console.log('error occured');
+        } else if (data) {
+            this.account = data;
+            console.log('Stefan');
+            console.log(this.account.fields.BillingCity.value);
+            
+            let address = this.account.fields.BillingCity.value;
+            if(!this.lastAddress) {
+                this.lastAddress = this.account.fields.BillingCity.value;
+            }
+            if (address != this.lastAddress) {
+                this.retrieveWeather();
+                this.showNotification();
+            }
+            
+        }
+    }
+
+    showNotification() {
+        const evt = new ShowToastEvent({
+            title: 'Success',
+            message: 'Account Billing Address Modified, going to trigger retrieveWeather',
+            variant: 'success'
+        });
+        this.dispatchEvent(evt);
+    }
+
 
     retrieveWeather(){
         retrieveWeather({accId: this.recordId}).then(response=>{
